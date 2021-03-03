10cd85bbfc31d8f3cf2bb4fade7e1bbde3d57e80
Suda Nimh <rslepejian@gmail.com>
2021-03-02 22:25:19 -0800

    all routes and controllers for yearbook set up but untested

---
+++ controllers/yearbookController.js
@@ -0,0 +1,41 @@
+const db = require("../models");
+
+// Defining methods for the yearbook controller
+module.exports = {
+    findAll: function (req, res) {
+        db.Yearbook.findAll(req.query)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function (req, res) {
+        db.Yearbook.findOne({ where: { id: req.params.id } })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findByYear: function (req, res) {
+        db.Yearbook.findAll({ where: { year: req.params.year } })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findyByUser: function (req, res) {
+        db.Yearbook.findAll({ where: { UserId: req.params.user } })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function (req, res) {
+        db.Yearbook.create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        db.Yearbook.update(req.body, { where: { id: req.body.id } })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function (req, res) {
+        db.Yearbook.destroy({ where: { id: req.params.id } })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+};
--- routes/api/yearbook.js
+++ routes/api/yearbook.js
@@ -1,56 +1,6 @@
 const router = require("express").Router();
 const yearbookController = require("../../controllers/yearbookController");
 
-app.get("/api/yearbooks/", function (req, res) {
-    db.Yearbooks.findAll().then(function (data) {
-        res.json(data);
-    });
-});
-
-//      get all yearbooks associated with a year
-
-app.get("/api/yearbooksyear/:year", function (req, res) {
-    db.Yearbooks.findAll({ where: { year: req.params.year } }).then(function (data) {
-        res.json(data);
-    });
-});
-
-//      get all yearbooks associated with a specific user
-
-app.get("/api/yearbookscreator/:id", function (req, res) {
-    db.Yearbooks.findAll({ where: { UserId: req.params.id } }).then(function (data) {
-        res.json(data);
-    });
-});
-
-//      get a yearbook by its id
-
-app.get("/api/yearbooksid/:id", function (req, res) {
-    db.Yearbooks.findOne({ where: { id: req.params.id } }).then(function (data) {
-        res.json(data);
-    });
-});
-
-//      update basic yearbook data
-
-app.put("/api/update/book", function (req, res) {
-    // Use the sequelize update method to update a todo to be equal to the value of req.body
-    // req.body will contain the id of the yearbook we need to update
-    db.Yearbook.update(
-        {
-            schoolName: req.body.name,
-            numberOfClasses: req.body.number,
-            year: req.body.year,
-            UserId: req.body.user
-        },
-        { where: { id: req.body.id } }
-    );
-    res.json(null);
-});
-
-
-
-
 // Matches with "/api/books"
 router
     .route("/")
