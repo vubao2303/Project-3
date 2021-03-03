e27b9c436bf21006f88deaca94b16575bec064e0
Suda Nimh <rslepejian@gmail.com>
2021-03-02 22:56:26 -0800

    deleted apiroutes.js

--- routes/api/student.js
+++ routes/api/student.js
@@ -1,49 +1,6 @@
 const router = require("express").Router();
 const studentController = require("../../controllers/studentController.js");
 
-// find person by id
-
-app.get("/api/students/:id", function (req, res) {
-    db.Students.findAll({ where: { ClassId: req.params.id } }).then(function (data) {
-        res.json(data);
-    });
-});
-
-// create a person (in a class) 
-app.post("/api/newPerson", function (req, res) {
-    db.Students.create({
-        name: req.body.name,
-        nickname: req.body.nickname,
-        hobbies: req.body.hobbies,
-        quote: req.body.quote,
-        linkedIn: req.body.linkedin,
-        title: req.body.title,
-        ClassId: req.body.class
-    }).then(function () {
-        res.json(null);
-    });
-});
-
-// update a person
-
-app.put("/api/update/person", function (req, res) {
-    // Use the sequelize update method to update a todo to be equal to the value of req.body
-    // req.body will contain the id of the yearbook we need to update
-    db.Class.update(
-        {
-            name: req.body.name,
-            nickname: req.body.nickname,
-            hobbies: req.body.hobbies,
-            quote: req.body.quote,
-            linkedIn: req.body.linkedin,
-            title: req.body.title,
-            ClassId: req.body.class
-        },
-        { where: { id: req.body.id } }
-    );
-    res.json(null);
-});
-
 // Matches with "/api/student"
 router
     .route("/")
