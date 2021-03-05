ef411ec737cd431113cad66cb39c4444173cb570
Jas-F <jasminer.franklin@gmail.com>
2021-03-04 22:18:59 -0500

    insert all params into tables

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -4,4 +4,8 @@ USE yearbook_db
 -- Import seeds for sql data base
 -- add parameters to add into yearbook 
 -- the order will insert into database
-INSERT INTO yearbook (schoolname, year, name)
+INSERT INTO yearbook (schoolname, year, name);
+
+INSERT INTO class (grade, name);
+
+INSERT INTO students (name, hobbies, quote, linkedIn, title);
