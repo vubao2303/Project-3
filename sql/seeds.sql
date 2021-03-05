e84834b95130b8684aae8333a5f5c1ddc682e5c8
Jas-F <jasminer.franklin@gmail.com>
2021-03-04 22:26:49 -0500

    create seeds

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -5,7 +5,13 @@ USE yearbook_db
 -- add parameters to add into yearbook 
 -- the order will insert into database
 INSERT INTO yearbook (schoolname, year, name);
+-- insert values
+VALUES ("UC Berkeley", 2021, "Bootcamp");
 
-INSERT INTO class (grade, name);
+INSERT INTO class (grade, name)
+-- insert class values
+VALUES ("13", "consoleloggers");
 
-INSERT INTO students (name, hobbies, quote, linkedIn, title);
+INSERT INTO students (name, hobbies, quote, linkedIn, title)
+-- insert studnets values
+VALUES ("B tram Vu", "yoga", "fake it till you make it", "https://www.linkedin.com/in/b-tram-vu/", student);
