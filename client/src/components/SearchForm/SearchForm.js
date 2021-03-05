6d1fce861f4a3fa114bd73cb619ede059f08f846
Suda Nimh <rslepejian@gmail.com>
2021-03-04 18:19:32 -0800

    fixed merge conflicts

---
+++ client/src/components/SearchForm/SearchForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function SearchForm() {
+  return (
+    <div>
+      <form >
+        <div className="mb-3">
+          <label for="searchforYearBook" className="form-label">Search</label>
+          <input placeholer="search for your yearbook here" className="form-control" id="searchForYB" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+
+}
+
+export default SearchForm;
