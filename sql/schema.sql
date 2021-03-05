da080bc12f4d70b2d00ecdc13b932173c5c26eb9
Jas-F <jasminer.franklin@gmail.com>
2021-03-04 21:34:08 -0500

    create db tables

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,4 +2,12 @@
 DROP DATABASE IF EXISTS yearbook_db;
 -- Creates the "blogger" database --
 CREATE DATABASE yearbook_db;
+USE yearbook_db;
+
+-- create new table define the characters values and rules
+CREATE TABLE yearbook
+
+CREATE TABLE classes
+
+CREATE TABLE students
 
---
+++ sql/seeds.sql
@@ -0,0 +1,6 @@
+-- Import seeds for sql data base
+INSERT INTO student (name, hobbies, quote, linkedIn, title) values ()
+
+INSERT INTO create (schoolName, year, name) values ()
+
+INSERT INTO class (grade, name) values ()
