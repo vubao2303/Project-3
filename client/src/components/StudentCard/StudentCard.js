fd590d376a6964ba8fef9e2e0bc9b08af7082fd1
vubao2303 <bbbvut@gmail.com>
2021-03-05 13:19:55 -0800

    add student card

---
+++ client/src/components/StudentCard/StudentCard.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function StudentCard() {
+  return (
+    <div>
+      <p> Year Book Data </p>
+      <p> Class Link </p>
+    </div>
+  )
+}
+export default StudentCard
