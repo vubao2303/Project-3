6f3538b333c00f4e27b1445c42b336305ace19ab
Suda Nimh <rslepejian@gmail.com>
2021-03-04 20:36:06 -0800

    updated dependencies, created signup function that adds a user to the db

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import API from "../../utils/API";
 
 function LoginForm() {
+
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    function signup(userName, passWord) {
+        console.log("triggered");
+        var userData = {username: userName, password: passWord};
+        API.signup(userData);
+    }
+
+    function login(userName, passWord) {
+        console.log("triggered");
+        var userData = {username: userName, password: passWord};
+        API.login(userData);
+    }
+
     return (
         <div>
             <form >
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(event) => {
+                        event.preventDefault();
+                        // console.log("banana")
+                        setName(event.target.value);
+                    }} />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <input type="password" className="form-control" id="exampleInputPassword1" onChange={(event) => {
+                        event.preventDefault();
+                        // console.log("banana")
+                        setPassword(event.target.value);
+                    }} />
                 </div>
 
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary" onClick = {(event) => 
+                    {event.preventDefault();
+                    login(name, password);}
+                    }>Login</button>
+                <button type="submit" className="btn btn-primary" onClick = {(event) => 
+                    {event.preventDefault();
+                    signup(name, password);}
+                    }>Signup</button>
             </form>
         </div>
     )
