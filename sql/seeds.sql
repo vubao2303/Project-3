dd51a42744c133b7edc8f92233e69b33e0752ac0
Jas-F <jasminer.franklin@gmail.com>
2021-03-05 18:21:34 -0500

    add mysql seeds

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -16,3 +16,7 @@ VALUES ("null", "consoleloggers")
 INSERT INTO students (name, nickname, hobbies, quote, linkedIn, title)
 -- insert studnets values
 VALUES ("B tram Vu", "Wasabli", "yoga", "fake it till you make it", "https://www.linkedin.com/in/b-tram-vu/", "student"),
+("Muhammad A Khalid", "M", "Games", "Life before dealth, strength before weakness, journey before destinationn", "https://www.linkedin.com/in/the-real-jordan-kelly/", "student"),
+("Ryan Kirkland", "kirl", "Games", "If you can't love yourself ho the hell are you gonna love yourself", "https://www.linkedin.com/in/b-tram-vu/", "student"),
+("Vincent Nquyen", "vin", "Games", "If you cant dazzle them with brilliance, baffle them with bullshit.", "https://www.linkedin.com/in/vincent-nguyen-74226a107/", "student"),
+("Vincent Nquyen", "vin", "Games", "If you cant dazzle them with brilliance, baffle them with bullshit.", "https://www.linkedin.com/in/vincent-nguyen-74226a107/", "student")
