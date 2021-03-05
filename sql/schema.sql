76ceff57a03b0002c991a924bfe134ed3cc4a9e2
Jas-F <jasminer.franklin@gmail.com>
2021-03-04 21:45:50 -0500

    create class table

--- sql/schema.sql
+++ sql/schema.sql
@@ -7,13 +7,20 @@ USE yearbook_db;
 -- create new table define the characters values and rules
 CREATE TABLE yearbook (
     -- auto increment id with 25 charater names
-    id INI NOT NULL AUTO_INCREMENT
-    name VARCHAR(25)
-    year INT NOT NULL
-    name VARCHAR(25)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(25),
+    year INT NOT NULL,
+    name VARCHAR(25),
+    PRIMARY KEY (id)
 )
 
-CREATE TABLE classes
+CREATE TABLE class (
+    -- AUTO INCREMENT & grade number
+    id INT NOT NULL AUTO_INCREMENT,
+    grade INT NOT NULL,
+    name VARCHAR(25),
+    PRIMARY KEY(id)
+)
 
 CREATE TABLE students
-
+    id INT NOT NULL AUTO_INCREMENT
