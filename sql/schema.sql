63853b35eab456617e46d0ad51cad1f5b6f42f37
Jas-F <jasminer.franklin@gmail.com>
2021-03-04 21:40:41 -0500

    create yearbook table

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,7 +5,13 @@ CREATE DATABASE yearbook_db;
 USE yearbook_db;
 
 -- create new table define the characters values and rules
-CREATE TABLE yearbook
+CREATE TABLE yearbook (
+    -- auto increment id with 25 charater names
+    id INI NOT NULL AUTO_INCREMENT
+    name VARCHAR(25)
+    year INT NOT NULL
+    name VARCHAR(25)
+)
 
 CREATE TABLE classes
 
