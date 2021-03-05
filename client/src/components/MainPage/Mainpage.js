76049f676c111e187ddc95a98a34000e8c3d4a1f
vubao2303 <bbbvut@gmail.com>
2021-03-05 01:17:25 -0800

    style buttons

--- client/src/components/MainPage/Mainpage.js
+++ client/src/components/MainPage/Mainpage.js
@@ -1,69 +1,39 @@
-// import React from "react";
-// import "./style.css";
-
-
-// function MainPage() {
-//   return (
-//     <div>
-//       <div className="hero-image">
-
-//         <div className="hero-text">
-//           <h1>Mnemosyne</h1>
-//           <p><h5>Collect Moments</h5></p>
-//           <button>Create Yearbook</button>
-//         </div>
-
-//       </div>
-//     </div>
-
-//   )
-// }
-
-// export default MainPage;
-
-
-// try this 
-
-
 import React from "react";
-
 import "./style.css";
-import { Link } from "react-router-dom";
 
 function MainPage() {
   return (
-
     <div className="hero-image">
       <div className="hero-text">
         <h1>Mnemosyne</h1>
-        <h2><small>CREATE MEMORY</small></h2>
-
+        <h2><small>CREATE MEMORY | MAKE A YEARBOOK  </small></h2>
         <br />
         <div className="mainLinks">
-
-
-          <Link to={"/login"}>
-            <button><a className="login-bnt">Login</a></button>
-          </Link>
-
-          <Link to={"/search"}>
-            <button><a className="bnt">Search</a></button>
-          </Link>
-
-          <Link to={"/Create"}>
-            <button><a className="create-bnt">Create</a></button>
-          </Link>
-
-        </div>
-
-        <div>
-
+          {/* Link to login  */}
+          <button><a href="/login">Login</a></button>
+          {/* Link to search */}
+          <button><a href="/search">Search</a></button>
+          {/* Link to create page  */}
+          <button><a href="/create">Create</a></button>
         </div>
       </div>
     </div>
-
-
   )
 }
 
 export default MainPage;
+
+
+// import { Link } from "react-router-dom";
+{/* this is not acccpetable because it doesn't like {}, but we may need this for deploy */ }
+{/* <Link to={"login"}>
+<button><a href="/login">Login</a></button>
+</Link>
+
+<Link to={"/search"}>
+<button><a className="bnt">Search</a></button>
+</Link>
+
+<Link to={"/Create"}>
+<button><a className="create-bnt">Create</a></button>
+</Link> */}
