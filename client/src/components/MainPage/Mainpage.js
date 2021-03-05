6640b0313f70910d2ec45633c73ccb4b050a6fe6
vubao2303 <bbbvut@gmail.com>
2021-03-04 23:41:34 -0800

    create a mainpage folder

---
+++ client/src/components/MainPage/Mainpage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+
+function Nav() {
+  return (
+    <div>
+      <header className="hero-image">
+
+        <div className="hero-text">
+          <h1>Mnemosyne</h1>
+          <p><h5>Collect Moments</h5></p>
+          <button>Create Yearbook</button>
+        </div>
+
+      </header>
+    </div>
+
+  )
+}
+
+export default Nav;
