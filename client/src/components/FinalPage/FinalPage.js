4946093f7fa76210c0cab2ab359df3792a485c61
Jas-F <jasminer.franklin@gmail.com>
2021-03-06 14:51:04 -0500

    add pseudo code

--- client/src/components/FinalPage/FinalPage.js
+++ client/src/components/FinalPage/FinalPage.js
@@ -1,1 +1,7 @@
+// create carosel compoenent using new javascript library slick
+// populate student details onto carosel displaying yearbook data 
+// add slick css script and link
+// install packages
+// format pages
+// import data from database into carosel
 import React from "react";
