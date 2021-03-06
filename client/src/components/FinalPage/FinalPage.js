af7d77805b4f5fd451640b15c9a4c77351b30815
Jas-F <jasminer.franklin@gmail.com>
2021-03-06 15:09:26 -0500

    update pseudo code

--- client/src/components/FinalPage/FinalPage.js
+++ client/src/components/FinalPage/FinalPage.js
@@ -4,9 +4,18 @@
 // install packages
 // format pages
 // import data from database into carosel
+// add on swipe event that creates a new slide with student data
 import React from "react";
 
 // create carosel with enough slides for each class member and staff
 <div data-slick='{"slidesToShow": 27, "slidesToScroll": 27}'>
+    <div>Place Holder Student</div>
+    <div>Place Holder Student</div>
+    <div>Place Holder Student</div>
+    <div>Place Holder Student</div>
+    <div>Place Holder Student</div>
+    <div>Place Holder Student</div>
+    <div>Place Holder Student</div>
+    <div>Place Holder Student</div>
 
 </div>
