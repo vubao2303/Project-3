b6c07c024b9fc7d09dcc983f3eb5e374a3306d22
Jas-F <jasminer.franklin@gmail.com>
2021-03-06 19:44:27 -0500

    props skeleton

--- client/src/components/StudentCard/StudentCard.js
+++ client/src/components/StudentCard/StudentCard.js
@@ -5,10 +5,14 @@ function StudentCard(props) {
     <div>
       return (
       <div className="container">
-
+{/* add props skeleton */}
         <div className="card mb-3">
           <h2>{props.name}</h2>
           <h3>Also known as {props.nickname}</h3>
+          <h2>Hobbies: {props.hobbies}</h2>
+          <h2>Quote: {props.quote}</h2>
+          <h2>Linkedin: {props.link}</h2>
+          <h2>{props.title}</h2>
           
         </div>
 
