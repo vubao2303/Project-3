024afe7cf198768f191eff0fe34310620a04b96d
Jas-F <jasminer.franklin@gmail.com>
2021-03-08 14:21:10 -0500

    import slick css

--- client/src/components/StudentCard/StudentCard.js
+++ client/src/components/StudentCard/StudentCard.js
@@ -62,3 +62,31 @@ function StudentCard(props) {
 
 
 export default StudentCard;
+
+// original code
+{/* <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+<div className="carousel-inner">
+  <div className="carousel-item active">
+    <div className="card mb-3">
+      <h2>{props.name}</h2>
+      <h3>Also known as {props.nickname}</h3>
+      <h4>Hobbies:</h4>
+      <p>{props.hobbies}</p>
+      <h4>Quote:</h4>
+      <p>{props.quote}</p>
+      <img alt="student" src={props.image} />
+      <h5>Link:</h5>
+      <a href={props.linkedIn} >See more info on this person!</a>
+    </div>
+  </div>
+
+</div>
+<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span className="visually-hidden">Previous</span>
+</button>
+<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+  <span className="visually-hidden">Next</span>
+</button>
+</div> */}
