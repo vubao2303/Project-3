3790677056419b789302f5dd0980a277ee007834
Suda Nimh <rslepejian@gmail.com>
2021-03-07 16:19:43 -0800

    now shows yearbooks associated with a user on the create page

--- client/src/components/UserPage/UserPage.js
+++ client/src/components/UserPage/UserPage.js
@@ -5,6 +5,37 @@
 // the create button will take me to the create page
 
 // the list of my year book will take me to the classes and yearbook page
+import React, { useState, useEffect } from "react"
+import API from "../../utils/API";
+// import "./style.css";
 
 
+function UserPage() {
 
+    const [books, setBooks] = useState([]);
+    const [user, setUser] = useState(sessionStorage.getItem("userId"));
+
+
+    useEffect(() => {
+        API.getBooksByUser(user).then((books) => {
+            setBooks(books.data);
+        })
+            .catch(err => console.log(err));
+      }, [user])
+
+    return (
+        <div>
+            <div className="searchResult">
+                {books.map((book, id) => {
+                    var href = "/display/" + book.id;
+                    return (
+                        <ul><li><a key={id} href={href}>{book.schoolName}, {book.year}</a></li></ul>
+                    )
+                })}
+            </div>
+        </div>
+    )
+
+}
+
+export default UserPage;
--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,9 +1,12 @@
 import React from "react"
 import CreateForm from '../components/CreateForm/CreateForm';
-
+import UserPage from '../components/UserPage/UserPage'
 function Create() {
   return (
-    <CreateForm />
+    <div>
+      <CreateForm />
+      <UserPage />
+    </div>
   )
 }
 export default Create
