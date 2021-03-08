8244ec7a95f1ea601e503e273c5c4b6662f0edef
Jas-F <jasminer.franklin@gmail.com>
2021-03-08 17:46:12 -0500

    finish carosel

--- client/src/components/StudentCard/StudentCard.js
+++ client/src/components/StudentCard/StudentCard.js
@@ -24,7 +24,81 @@
 // create var of items that host the student data from props
 // create carousel functionality
 // return item data indside of carosel component
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 
+
+
+const StudentCard = (props) => {
+
+  const items = props.students
+
+  // const items = [
+  //   {
+  //     image: props.image,
+  //     name: props.name,
+  //     nickname: props.nickname,
+  //     hobbies: props.hobbies,
+  //     quote: props.quote
+
+  //   }
+
+  // ];
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item}
+      >
+        <img src={item.image} alt={item.altText} />
+
+        <CarouselCaption captionText={item.quote} captionHeader={item.name} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
+}
+
+export default StudentCard;
 
 //   // CarouselCaption
 // } from 'reactstrap';
@@ -51,7 +125,7 @@
 //       <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
 //     </Carousel> */}
 
-   
+
 //   );
 // }
 
